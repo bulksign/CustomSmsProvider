866c24b2fc1332ab8037a6b46065aa319af640de
Bulksign <contact@bulksign.com>
2017-08-04 18:31:14 +0300

    added sample code
    added sample code

---
+++ README.md
@@ -0,0 +1,11 @@
+# CustomSmsProvider for Bulksign platform
+This is sample code that demonstrates how to implement a custom sms provider for the [Bulksign](https://bulksign.com) platform.
+
+The code itself is very simple. This references the Bulksign.Extensibility library from NuGet and contains a class that implements the ISmsProvider interface. 
+
+```
+public class MyCustomSmsProvider : ISMSProvider
+```
+
+
+
---
+++ src/CustomSmsProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Bulksign.Extensibility;
+
+namespace CustomSmsProvider
+{
+    public class MyCustomSmsProvider : ISMSProvider
+    {
+        public event LogDelegate Log;
+
+        public SMSResult Send(string text, string phoneNumber)
+        {
+
+            try
+            {
+
+                //add code to send sms
+
+
+
+
+                //successful result
+                return new SMSResult()
+                {
+                    SMSIdentifier = Guid.NewGuid().ToString(),
+                    ErrorMessage = string.Empty,
+                    IsSuccess = true
+                };
+
+            }
+            catch (Exception ex)
+            {
+                //failed
+                return new SMSResult()
+                {
+                    SMSIdentifier = string.Empty,
+                    ErrorMessage = ex.Message,
+                    IsSuccess = false
+                };
+
+            }
+
+        }
+
+        public string ProviderName => "CustomSmsProvider";
+    }
+}
