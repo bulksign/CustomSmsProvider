18a08da1eac77cfd71f80527a5b27a9cf8bb7aea
Bulksign <contact@bulksign.com>
2020-03-05 12:26:32 +0200

    updated sample to latest Bulksign.Extensibility SDK version

--- src/CustomSmsProvider.cs
+++ src/CustomSmsProvider.cs
@@ -1,46 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 using Bulksign.Extensibility;
 
 namespace CustomSmsProvider
 {
-    public class MyCustomSmsProvider : ISMSProvider
-    {
-        public event LogDelegate Log;
+	public class MyCustomSmsProvider : ISMSProvider
+	{
+		public event LogDelegate Log;
 
-        public SMSResult Send(string text, string phoneNumber)
-        {
+		public SMSResult Send(string text, string phoneNumber)
+		{
 
-            try
-            {
+			try
+			{
 
-                //add code to send sms
+				Log(LogLevel.Info, null, $"Started sending SMS using provider {ProviderName} ");
+
+				//add code to send sms using the provider/gateway you want 
 
 
 
+				//successful result
+				return new SMSResult()
+				{
+					Identifier = Guid.NewGuid().ToString(),
+					ErrorMessage = string.Empty,
+					IsSuccess = true
+				};
 
-                //successful result
-                return new SMSResult()
-                {
-                    SMSIdentifier = Guid.NewGuid().ToString(),
-                    ErrorMessage = string.Empty,
-                    IsSuccess = true
-                };
+			}
+			catch (Exception ex)
+			{
+				//failed
+				return new SMSResult()
+				{
+					Identifier = string.Empty,
+					ErrorMessage = ex.Message,
+					IsSuccess = false
+				};
 
-            }
-            catch (Exception ex)
-            {
-                //failed
-                return new SMSResult()
-                {
-                    SMSIdentifier = string.Empty,
-                    ErrorMessage = ex.Message,
-                    IsSuccess = false
-                };
+			}
 
-            }
+		}
 
-        }
+		public Dictionary<string, string> Settings
+		{
+			get;
+			set;
+		}
 
-        public string ProviderName => "CustomSmsProvider";
-    }
+		public string ProviderName => "CustomSmsProvider";
+	}
 }
